5db31a9c857dd6405105c890b890bcef70c53cf5
Vitali <37334443+v1t3@users.noreply.github.com>
2020-10-03 21:37:10 +0300

    Исправлено закрытие меню в мобилке

--- assets/js/components/menu_mobile.vue
+++ assets/js/components/menu_mobile.vue
@@ -3,25 +3,25 @@
         <nav role="navigation" v-on-clickaway="closeMenu">
             <div id="menuToggle">
                 <div class="burger" @click.prevent="showMenu" v-bind:class="{ active: isActive }">
-                    <span></span>
-                    <span></span>
-                    <span></span>
+                    <span class="burger-line"></span>
+                    <span class="burger-line"></span>
+                    <span class="burger-line"></span>
                 </div>
                 <ul id="menu">
                     <li><a href="/">Главная</a></li>
                     <hr/>
                     <li>
-                        <router-link :to="{ name: 'home' }">Сводка</router-link>
+                        <router-link :to="{ name: 'home' }"><span @click="closeMenu">Сводка</span></router-link>
                     </li>
                     <li>
-                        <router-link :to="{ name: 'Channel_data' }">Каналы</router-link>
+                        <router-link :to="{ name: 'Channel_data' }"><span @click="closeMenu">Каналы</span></router-link>
                     </li>
                     <li>
-                        <router-link :to="{ name: 'Coubdata' }">Коубы</router-link>
+                        <router-link :to="{ name: 'Coubdata' }"><span @click="closeMenu">Коубы</span></router-link>
                     </li>
                     <hr/>
                     <li>
-                        <router-link :to="{ name: 'Settings' }">Настройки</router-link>
+                        <router-link :to="{ name: 'Settings' }"><span @click="closeMenu">Настройки</span></router-link>
                     </li>
                 </ul>
             </div>
