77b2ab24d41ecb4535df9e15b9f24b4353d122b9
Vitali <37334443+v1t3@users.noreply.github.com>
2020-10-09 22:24:09 +0300

    Добавлен хендлер Access Denied

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    public function handle(Request $request, AccessDeniedException $accessDeniedException)
+    {
+        $content = 'Доступ запрещён';
+
+        return new Response($content, 403);
+    }
+}
